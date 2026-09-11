ebb4591ff0dd9044d2ac20fbd0241b194a73a73e
Meg-Diom <d.ngum1@alustudent.com>
2026-09-11 13:39:18 +0400

    Main module for command line application

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ persistence, processing and validation logic:
 
 The program displays a menu and allows the user to add, view, update,
 delete and search expenses, as well as view totals and category totals.
+The program also displays a munu for managing saving goals
 
 Usage:
     python main.py
@@ -39,6 +40,7 @@ def main():
     # Load persisted data (creates a dict with a "transactions" list)
     data = data_store.load_data()
     transactions = data["transactions"]
+    saving_goals = data["saving_goals"]
 
     # Main interactive loop — repeats until the user selects Exit
     while True:
@@ -47,7 +49,7 @@ def main():
         print("===== PERSONAL BUDGET TRACKER =====")
         print(40 * "-")
         print("1. Add expense\n2. View expense\n3. Update expense\n4. Delete expense")
-        print("5. Search expense\n6. Show total expense\n7. Show expense by category\n8. Exit")
+        print("5. Search expense\n6. Show total expense\n7. Show expense by category\n8. Manage saving goals\n9. Exit")
 
         # Read the user's selection
         option = input("Enter your option: ")
@@ -298,223 +300,223 @@ def main():
                     print("{}: {:.2f}".format(category, total))
             else:
                 print("No expenses found!")
-
+        
         elif option == "8":
-
-        while True:
-
-            print("\n===== SAVING GOALS =====")
-            print("1. Add saving goal")
-            print("2. View saving goals")
-            print("3. Update saving goal")
-            print("4. Delete saving goal")
-            print("5. Show saving progress")
-            print("6. Back to main menu")
-
-            goal_option = input("Enter your option: ")
-
-            if goal_option == "1":
-
-                while True:
-                    user_id = input("Enter your user ID: ")
-                    user_id = validation.validate_user_id(user_id)
-
-                    if user_id is not None:
-                        break
-
-                    print("This field cannot be empty!")
-
-                while True:
-                    goal = input("Enter your saving goal: ")
-                    goal = validation.validate_goal(goal)
-
-                    if goal is not None:
-                        break
-
-                    print("This field cannot be empty!")
-
-                while True:
-                    target_amount = input("Enter target amount: ")
-                    target_amount = validation.validate_target_amount(target_amount)
-
-                    if target_amount is not None:
-                        break
-
-                    print("Target amount must be greater than 0!")
-
-                while True:
-                    saved_amount = input("Enter amount already saved: ")
-                    saved_amount = validation.validate_amount(saved_amount)
-
-                    if saved_amount is not None:
-                        if saved_amount <= target_amount:
-                            break
-
-                    print("Invalid saved amount!")
-
-                while True:
-                    deadline = input("Enter deadline (dd/mm/yyyy): ")
-                    deadline = validation.validate_date(deadline)
-
-                    if deadline is not None:
-                        break
-
-                    print("Invalid date!")
-
-                saving_goals = processing.add_saving_goal(
-                    saving_goals,
-                    user_id,
-                    goal,
-                    target_amount,
-                    saved_amount,
-                    deadline
-                )
-
-                data["saving_goals"] = saving_goals
-                data_store.save_data(data)
-
-                print("Saving goal added successfully!")
-            elif goal_option == "2":
-
-                if saving_goals:
-                    print("\n===== SAVING GOALS =====")
-
-                    for saving_goal in saving_goals:
-                        print(saving_goal)
-                else:
-                    print("No saving goals found!")
-
-            elif goal_option == "3":
-
-                while True:
-                    user_id = input("Enter your user ID: ")
-                    user_id = validation.validate_user_id(user_id)
-
-                    if user_id is not None:
-                        break
-
-                    print("This field cannot be empty!")
-                goal = input("Enter the goal name: ")
-
-                results = processing.find_saving_goal(saving_goals, user_id, goal)
-
-                if not results:
-                    print("Saving goal not found!")
-
-                else:
-                    for saving_goal in results:
-                        print(saving_goal)
-
-                    print("1. Update goal")
-                    print("2. Update target amount")
-                    print("3. Update saved amount")
-                    print("4. Update deadline")
-
-                    field = input("Enter the field you want to update: ")
-
-                    if field == "1":
-                        modify = input("Enter the new goal: ")
-                        modify = validation.validate_goal(modify)
-
-                    elif field == "2":
-                         while True:
-                            modify = input("Enter the new target amount: ")
-                            modify = validation.validate_target_amount(modify)
-
-                            if modify is not None:
-                                current_saved = results[0]["saved_amount"]
-
-                                if modify >= current_saved:
-                                    break
-
-                            print("Target amount cannot be less than the amount already saved!")
-
-
-                    elif field == "3":
-                        while True:
-                            modify = input("Enter the new saved amount: ")
-                            modify = validation.validate_amount(modify)
-
-                            if modify is not None:
-                                current_target = results[0]["target_amount"]
-
-                                if modify <= current_target:
-                                    break
-
-                            print("Saved amount cannot be greater than the target amount!")
-
-                    elif field == "4":
-                        modify = input("Enter the new deadline: ")
-                        modify = validation.validate_date(modify)
-
-                    else:
-                        print("Invalid field!")
-                        modify = None
-
-                    if modify is not None:
-                        saving_goals = processing.update_saving_goal(saving_goals, user_id, goal, field, modify)
-
-                        data["saving_goals"] = saving_goals
-                        data_store.save_data(data)
-
-                        print("Saving goal updated successfully!")
-
-
-            elif goal_option == "4":
-
-                while True:
-                    user_id = input("Enter your user ID: ")
-                    user_id = validation.validate_user_id(user_id)
-
-                    if user_id is not None:
-                        break
-
-                    print("This field cannot be empty!")
-                goal = input("Enter the goal name: ")
-
-                results = processing.find_saving_goal(saving_goals, user_id, goal)
-
-                if not results:
-                    print("Saving goal not found!")
-
-                else:
-                    for saving_goal in results:
-                        print(saving_goal)
-
-                    saving_goals = processing.delete_saving_goal(saving_goals, user_id, goal)
-
-                    data["saving_goals"] = saving_goals
-                    data_store.save_data(data)
-
-                    print("Saving goal deleted successfully!")
-
-
-            elif goal_option == "5":
-
-                if not saving_goals:
-                    print("No saving goals found!")
-
-                else:
-                    progress = processing.calculate_saving_progress(
-                        saving_goals
-                    )
-
-                    print("\n===== SAVING PROGRESS =====")
-
-                    for result in progress:
-                        print("Goal: {}".format(result["goal"]))
-                        print("Progress: {:.2f}%".format(result["progress"]))
-
-                        if result["progress"] >= 100:
-                            print("Goal completed!")
-
-                        print("-" * 30)
-
-
-            elif goal_option == "6":
-                    break
-            else:
-                print("Invalid option!")
+        #Option 8 : Provides an inteface for the  user to track their saving goals and display the saving progress
+          while True:
+  
+              print("\n===== SAVING GOALS =====")
+              print("1. Add saving goal")
+              print("2. View saving goals")
+              print("3. Update saving goal")
+              print("4. Delete saving goal")
+              print("5. Show saving progress")
+              print("6. Back to main menu")
+  
+              goal_option = input("Enter your option: ")
+  
+              if goal_option == "1":
+  
+                  while True:
+                      user_id = input("Enter your user ID: ")
+                      user_id = validation.validate_user_id(user_id)
+  
+                      if user_id is not None:
+                          break
+  
+                      print("This field cannot be empty!")
+  
+                  while True:
+                      goal = input("Enter your saving goal: ")
+                      goal = validation.validate_goal(goal)
+  
+                      if goal is not None:
+                          break
+  
+                      print("This field cannot be empty!")
+  
+                  while True:
+                      target_amount = input("Enter target amount: ")
+                      target_amount = validation.validate_target_amount(target_amount)
+  
+                      if target_amount is not None:
+                          break
+  
+                      print("Target amount must be greater than 0!")
+  
+                  while True:
+                      saved_amount = input("Enter amount already saved: ")
+                      saved_amount = validation.validate_amount(saved_amount)
+  
+                      if saved_amount is not None:
+                          if saved_amount <= target_amount:
+                              break
+  
+                      print("Invalid saved amount!")
+  
+                  while True:
+                      deadline = input("Enter deadline (dd/mm/yyyy): ")
+                      deadline = validation.validate_date(deadline)
+  
+                      if deadline is not None:
+                          break
+  
+                      print("Invalid date!")
+  
+                  saving_goals = processing.add_saving_goal(
+                      saving_goals,
+                      user_id,
+                      goal,
+                      target_amount,
+                      saved_amount,
+                      deadline
+                  )
+  
+                  data["saving_goals"] = saving_goals
+                  data_store.save_data(data)
+  
+                  print("Saving goal added successfully!")
+              elif goal_option == "2":
+  
+                  if saving_goals:
+                      print("\n===== SAVING GOALS =====")
+  
+                      for saving_goal in saving_goals:
+                          print(saving_goal)
+                  else:
+                      print("No saving goals found!")
+  
+              elif goal_option == "3":
+  
+                  while True:
+                      user_id = input("Enter your user ID: ")
+                      user_id = validation.validate_user_id(user_id)
+  
+                      if user_id is not None:
+                          break
+  
+                      print("This field cannot be empty!")
+                  goal = input("Enter the goal name: ")
+  
+                  results = processing.find_saving_goal(saving_goals, user_id, goal)
+  
+                  if not results:
+                      print("Saving goal not found!")
+  
+                  else:
+                      for saving_goal in results:
+                          print(saving_goal)
+  
+                      print("1. Update goal")
+                      print("2. Update target amount")
+                      print("3. Update saved amount")
+                      print("4. Update deadline")
+  
+                      field = input("Enter the field you want to update: ")
+  
+                      if field == "1":
+                          modify = input("Enter the new goal: ")
+                          modify = validation.validate_goal(modify)
+  
+                      elif field == "2":
+                           while True:
+                              modify = input("Enter the new target amount: ")
+                              modify = validation.validate_target_amount(modify)
+  
+                              if modify is not None:
+                                  current_saved = results[0]["saved_amount"]
+  
+                                  if modify >= current_saved:
+                                      break
+  
+                              print("Target amount cannot be less than the amount already saved!")
+  
+  
+                      elif field == "3":
+                          while True:
+                              modify = input("Enter the new saved amount: ")
+                              modify = validation.validate_amount(modify)
+  
+                              if modify is not None:
+                                  current_target = results[0]["target_amount"]
+  
+                                  if modify <= current_target:
+                                      break
+  
+                              print("Saved amount cannot be greater than the target amount!")
+  
+                      elif field == "4":
+                          modify = input("Enter the new deadline: ")
+                          modify = validation.validate_date(modify)
+  
+                      else:
+                          print("Invalid field!")
+                          modify = None
+  
+                      if modify is not None:
+                          saving_goals = processing.update_saving_goal(saving_goals, user_id, goal, field, modify)
+  
+                          data["saving_goals"] = saving_goals
+                          data_store.save_data(data)
+  
+                          print("Saving goal updated successfully!")
+  
+  
+              elif goal_option == "4":
+  
+                  while True:
+                      user_id = input("Enter your user ID: ")
+                      user_id = validation.validate_user_id(user_id)
+  
+                      if user_id is not None:
+                          break
+  
+                      print("This field cannot be empty!")
+                  goal = input("Enter the goal name: ")
+  
+                  results = processing.find_saving_goal(saving_goals, user_id, goal)
+  
+                  if not results:
+                      print("Saving goal not found!")
+  
+                  else:
+                      for saving_goal in results:
+                          print(saving_goal)
+  
+                      saving_goals = processing.delete_saving_goal(saving_goals, user_id, goal)
+  
+                      data["saving_goals"] = saving_goals
+                      data_store.save_data(data)
+  
+                      print("Saving goal deleted successfully!")
+  
+  
+              elif goal_option == "5":
+  
+                  if not saving_goals:
+                      print("No saving goals found!")
+  
+                  else:
+                      progress = processing.calculate_saving_progress(
+                          saving_goals
+                      )
+  
+                      print("\n===== SAVING PROGRESS =====")
+  
+                      for result in progress:
+                          print("Goal: {}".format(result["goal"]))
+                          print("Progress: {:.2f}%".format(result["progress"]))
+  
+                          if result["progress"] >= 100:
+                              print("Goal completed!")
+  
+                          print("-" * 30)
+  
+  
+              elif goal_option == "6":
+                      break
+              else:
+                  print("Invalid option!")
     elif option == "9":
         break
     else:
